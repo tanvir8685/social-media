b43f139c22cc17fb51d23e5861b8bd4e1eae3fa0
Tanvirul Islam <tanvirhridoy1011@gmail.com>
2022-12-30 21:32:01 +0600

    adding comment

--- src/Pages/Media/ShowSts.js
+++ src/Pages/Media/ShowSts.js
@@ -1,9 +1,52 @@
+import { useQuery } from '@tanstack/react-query';
 import React from 'react';
 
 import { FaHeart ,FaComment} from "react-icons/fa";
 import { Link } from 'react-router-dom';
 const ShowSts = ({sts}) => {
     const {status,image,userName,_id}=sts;
+    console.log('id',sts)
+    const { data: allComment = [],refetch,isLoading } = useQuery({
+      queryKey: ['userDetail'],
+      queryFn: async () => {
+          const res = await fetch(`http://localhost:5000/allcomment/${_id}`);
+          const data = await res.json();
+          return data;
+          
+      }
+  });
+  console.log('comment',allComment)
+    const handleComment=(event)=>{
+
+      event.preventDefault();
+      const form = event.target;
+      const comment = form.comment.value;
+      
+      
+      const commentForDb={
+        commentId:_id,
+        commentBody:comment
+
+      }
+      fetch('http://localhost:5000/allcomment', {
+            method: 'POST',
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify(commentForDb)
+        })
+            .then(res => res.json())
+            .then(data => {
+                if (data.acknowledged) {
+                    form.reset();
+                    // refetch()
+                }
+                console.log(data)
+            })
+            .catch(err => console.error(err))
+      console.log(commentForDb)
+
+    }
     return (
         <div>
             <div className="card mt-5  w-96 bg-base-100 shadow-xl">
@@ -21,15 +64,38 @@ const ShowSts = ({sts}) => {
     <p><FaComment></FaComment></p>
     </div>
     <div className="form-control">
+    
   <div className="input-group">
+  <form className='ml-1' onSubmit={handleComment}>
+                <div className="form-control">
+                    <label className="input-group input-group-md">
+                       
+                        <input  type="text" name='comment' placeholder="Type here" className="input input-bordered input-md" />
+
+                        
+                           <button className='btn btn-primary ml-3'>Submit</button> 
+                        
+
+
+                    </label>
+                    
+                </div>
+            </form>
+
+
+    {/* <form onSubmit={handleComment()}>
     <input type="text" placeholder="Search…" className="input input-bordered" />
-    <button className="btn ">
+    <button  className="btn ">
       Comment
     </button>
+    </form> */}
+
   </div>
 </div>
   </div>
-  
+  {
+    allComment.map(cmnt=><p>{cmnt.commentBody}</p>)
+  }
 </div>
         </div>
     );
